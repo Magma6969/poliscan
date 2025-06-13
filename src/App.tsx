005195e151376b3922d1fc1c261ec5ab2cd09a66
Aaryan <1107aaryantiwari@gmail.com>
2025-06-14 00:28:07 +0530

    Fix: Update deployment with correct asset paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,10 @@
 import React from 'react';
-import { ChakraProvider, ColorModeScript, Box, CSSReset, Container, Heading, Button } from '@chakra-ui/react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { ChakraProvider, ColorModeScript, Box, CSSReset } from '@chakra-ui/react';
+import { Routes, Route } from 'react-router-dom';
 import theme from './styles/theme';
 import HomePage from './pages/HomePage';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-
-// Error Boundary component
-class ErrorBoundary extends React.Component<{children: React.ReactNode}, {hasError: boolean}> {
-  constructor(props: {children: React.ReactNode}) {
-    super(props);
-    this.state = { hasError: false };
-  }
-
-  static getDerivedStateFromError() {
-    return { hasError: true };
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return (
-        <Container centerContent mt={10}>
-          <Heading mb={4}>Something went wrong</Heading>
-          <Button colorScheme="purple" onClick={() => window.location.href = '/poliscan/'}>
-            Go to Home
-          </Button>
-        </Container>
-      );
-    }
-
-    return this.props.children;
-  }
-}
 
 function App() {
   return (
@@ -41,19 +14,10 @@ function App() {
       <Box minH="100vh" display="flex" flexDirection="column">
         <Navbar />
         <Box as="main" flex="1">
-          <ErrorBoundary>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="*" element={
-                <Container centerContent mt={10}>
-                  <Heading mb={4}>404 - Page Not Found</Heading>
-                  <Button as="a" href="/poliscan/" colorScheme="purple">
-                    Go to Home
-                  </Button>
-                </Container>
-              } />
-            </Routes>
-          </ErrorBoundary>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            {/* Add more routes as needed */}
+          </Routes>
         </Box>
         <Footer />
       </Box>
